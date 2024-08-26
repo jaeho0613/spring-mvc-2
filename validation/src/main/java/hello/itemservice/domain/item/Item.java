9700be1f4187eb84b40f9e7ac2604b010b381983
jaeho0613 <ywm2004@gmail.com>
2024-08-26 22:52:35 +0900

    #섹션6 - Bean Validation 오브젝트 오류

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+// 기능이 약해서 권장하지 않음
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원이 넘게 해주세요.")
 public class Item {
 
     private Long id;
