2fa1912d30d5eb845c91c79aeee15da2fc920439
jaeho0613 <ywm2004@gmail.com>
2024-09-08 02:08:32 +0900

    #섹션 11 포맷터 적용하기

--- typeconvert/src/main/java/hello/typeconvert/WebConfig.java
+++ typeconvert/src/main/java/hello/typeconvert/WebConfig.java
@@ -1,9 +1,8 @@
 package hello.typeconvert;
 
-import hello.typeconvert.converter.IntegerToStringConverter;
 import hello.typeconvert.converter.IpPortToStringConvert;
-import hello.typeconvert.converter.StringToIntegerConverter;
 import hello.typeconvert.converter.StringToIpPortConverter;
+import hello.typeconvert.formatter.MyNumberFormatter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,9 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToIntegerConverter());
-        registry.addConverter(new IntegerToStringConverter());
+        // 주석처리 우선순위
+        // registry.addConverter(new StringToIntegerConverter());
+        // registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
         registry.addConverter(new IpPortToStringConvert());
+
+        // 추가
+        registry.addFormatter(new MyNumberFormatter());
     }
 }
