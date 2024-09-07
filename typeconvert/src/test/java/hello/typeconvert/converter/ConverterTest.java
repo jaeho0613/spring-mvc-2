7044770947f49b5ae9c0786a4a9a5c5f72400a91
jaeho0613 <ywm2004@gmail.com>
2024-09-08 01:25:18 +0900

    #섹션 11 타입 컨버터 - Converter

---
+++ typeconvert/src/test/java/hello/typeconvert/converter/ConverterTest.java
@@ -0,0 +1,39 @@
+package hello.typeconvert.converter;
+
+import hello.typeconvert.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+    
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+    
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+    
+    @Test
+    void stringToIpPort() {
+        IpPortToStringConvert convert = new IpPortToStringConvert();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = convert.convert(source);
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void ipPortToString() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+}
